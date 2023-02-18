5189a81fb442fe40f9f4c723a22c86187f907f39
RomWeb3 <mspulco07@gmail.com>
2023-02-18 23:48:07 +0100

    commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        violet: "#AD1FEA",
+        blue: "#4661E6",
+        verydarkblue: "#373F68",
+        lightgray: "#F2F4FF",
+        verylightgray: "#F7F8FD",
+        darkblue: "#3A4374",
+        gray: "#647196",
+        orange: "#F49F85",
+        lightblue: "#62BCFA",
+        gradient:
+          "radial-gradient(128.88% 128.88% at 103.9% -10.39%, #E84D70 0%, #A337F6 53.09%, #28A7ED 100%)",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };

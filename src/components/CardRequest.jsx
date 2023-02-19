dd50f66b71b26c73600428266dd96c8242852ea5
RomWeb3 <mspulco07@gmail.com>
2023-02-19 12:49:36 +0100

    commit

--- src/components/CardRequest.jsx
+++ src/components/CardRequest.jsx
@@ -3,18 +3,24 @@ import React from "react";
 function CardRequest() {
   return (
     <div className="bg-white w-full px-6 py-6 rounded-[10px]">
-      <h2>Add tags for solutions</h2>
-      <p>Easier to search for solutions based on a specific stack.</p>
-      <div>Enhancement</div>
+      <h2 className="font-bold text-darkblue text-sm">
+        Add tags for solutions
+      </h2>
+      <p className="my-2 text-gray text-sm">
+        Easier to search for solutions based on a specific stack.
+      </p>
+      <div className="mb-4 bg-lightgray w-[111px] h-[30px] flex justify-center items-center rounded-[10px] text-blue font-semibold text-sm">
+        Enhancement
+      </div>
       <div className="flex justify-between items-center">
-        <div className="bg-lightgray flex justify-center items-center gap-2.5 rounded-[10px]">
+        <div className="bg-lightgray flex justify-center items-center gap-2.5 rounded-[10px] w-[69px] h-[32px] text-sm font-bold text-darkblue">
           <img
             src="../public/assets/shared/icon-arrow-up.svg"
             alt="icon arrow up"
           />
           112
         </div>
-        <div className="flex items-center gap-1">
+        <div className="flex items-center gap-1 text-sm font-bold text-darkblue">
           <img
             src="../public/assets/shared/icon-comments.svg"
             alt="icon comments"

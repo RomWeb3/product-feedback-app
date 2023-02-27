55d51ce6cf1afa2f976dd05bd957d5833987c196
RomWeb3 <mspulco07@gmail.com>
2023-02-27 20:31:03 +0100

    responsive Cardrequest done

--- src/components/CardRequest.jsx
+++ src/components/CardRequest.jsx
@@ -28,9 +28,9 @@ function CardRequest({ productRequest, datas, setDatas, screenWidth }) {
   //   setDatas({ ...datas, productRequest: updatedProductRequests });
   // };
 
-  return screenWidth < 768 ? (
+  return (
     <div
-      className="w-full max-w-[825px] bg-white px-6 py-6"
+      className="w-full max-w-[825px] bg-white px-6 md:px-0 py-6 md:py-[28px] md:relative"
       style={{
         borderTop:
           productRequest.status === "planned"
@@ -67,23 +67,25 @@ function CardRequest({ productRequest, datas, setDatas, screenWidth }) {
         </div>
       )}
       <div
-        className="cursor-pointer text-darkblue hover:text-[#4661E6]"
+        className="cursor-pointer text-darkblue hover:text-[#4661E6] md:ml-[112px]"
         onClick={() =>
           navigate(`/product-feedback-app/feedback/${productRequest.id}`)
         }
       >
-        <h2 className="font-bold transition-all text-sm">
+        <h2 className="font-bold transition-all text-sm md:text-lg">
           {productRequest.title}
         </h2>
-        <p className="my-2 text-gray text-sm">{productRequest.description}</p>
-        <div className="mb-4 bg-lightgray w-[111px] h-[30px] flex justify-center items-center rounded-[10px] text-blue font-semibold text-sm">
+        <p className="mt-2 mb-[10px] md:mb-3 text-gray text-sm md:text-base">
+          {productRequest.description}
+        </p>
+        <div className="mb-4 md:mb-0 bg-lightgray w-[111px] h-[30px] flex justify-center items-center rounded-[10px] text-blue font-semibold text-sm">
           {productRequest.category.charAt(0).toUpperCase() +
             productRequest.category.slice(1)}
         </div>
       </div>
       <div className="flex justify-between items-center">
         <button
-          className="bg-lightgray hover:bg-[#CFD7FF] cursor-pointer transition-all flex justify-center items-center gap-2.5 rounded-[10px] w-[69px] h-[32px] text-sm font-bold text-darkblue"
+          className="md:absolute md:top-[28px] md:left-[32px] bg-lightgray hover:bg-[#CFD7FF] w-[69px] md:w-10 h-[32px] md:h-[53px] cursor-pointer transition-all flex md:flex-col justify-center items-center gap-2.5 md:gap-2 rounded-[10px] text-sm font-bold text-darkblue"
           style={{ background: voted && "#4661E6", color: voted && "#fff" }}
           onClick={() => {
             onVote();
@@ -100,91 +102,7 @@ function CardRequest({ productRequest, datas, setDatas, screenWidth }) {
           />
           {productRequest.upvotes}
         </button>
-        <div className="flex items-center gap-1 text-sm font-bold text-darkblue">
-          <img src="/assets/shared/icon-comments.svg" alt="icon comments" />
-          {productRequest.comments?.length > 0
-            ? productRequest.comments.length +
-              productRequest.comments?.reduce(
-                (total, comment) => total + (comment.replies?.length || 0),
-                0
-              )
-            : 0}
-        </div>
-      </div>
-    </div>
-  ) : (
-    <div
-      className="w-full max-w-[825px] bg-white px-8 py-7 flex gap-10"
-      style={{
-        borderTop:
-          productRequest.status === "planned"
-            ? "6px solid #F49F85"
-            : productRequest.status === "in-progress"
-            ? "6px solid #AD1FEA"
-            : productRequest.status === "live"
-            ? "6px solid #62BCFA"
-            : "none",
-        borderRadius:
-          productRequest.status === "planned" ||
-          productRequest.status === "in-progress" ||
-          productRequest.status === "live"
-            ? "5px 5px 10px 10px"
-            : "10px",
-      }}
-    >
-      {productRequest.status === "planned" && (
-        <div className="flex items-center gap-4 mb-4">
-          <div className="w-2 h-2 bg-orange rounded-full"></div>
-          <p className="text-gray text-sm">Planned</p>
-        </div>
-      )}
-      {productRequest.status === "in-progress" && (
-        <div className="flex items-center gap-4 mb-4">
-          <div className="w-2 h-2 bg-violet rounded-full"></div>
-          <p className="text-gray text-sm">In Progress</p>
-        </div>
-      )}
-      {productRequest.status === "live" && (
-        <div className="flex items-center gap-4 mb-4">
-          <div className="w-2 h-2 bg-lightblue rounded-full"></div>
-          <p className="text-gray text-sm">live</p>
-        </div>
-      )}
-      <button
-        className="bg-lightgray hover:bg-[#CFD7FF] cursor-pointer transition-all flex flex-col justify-center items-center gap-2 rounded-[10px] w-10 h-[53px] text-sm font-bold text-darkblue"
-        style={{ background: voted && "#4661E6", color: voted && "#fff" }}
-        onClick={() => {
-          onVote();
-        }}
-      >
-        <img
-          src={
-            voted
-              ? "/assets/shared/icon-arrow-up-white.svg"
-              : "/assets/shared/icon-arrow-up.svg"
-          }
-          alt="icon arrow up"
-        />
-        {productRequest.upvotes}
-      </button>
-      <div className="w-full flex justify-between items-center">
-        <div
-          className="cursor-pointer text-darkblue hover:text-[#4661E6]"
-          onClick={() =>
-            navigate(`/product-feedback-app/feedback/${productRequest.id}`)
-          }
-        >
-          <h2 className="font-bold transition-all text-lg">
-            {productRequest.title}
-          </h2>
-          <p className="my-2 text-gray">{productRequest.description}</p>
-          <div className=" bg-lightgray w-[111px] h-[30px] flex justify-center items-center rounded-[10px] text-blue font-semibold text-sm">
-            {productRequest.category.charAt(0).toUpperCase() +
-              productRequest.category.slice(1)}
-          </div>
-        </div>
-
-        <div className="flex items-center gap-1 text-sm font-bold text-darkblue justify-self-end">
+        <div className="md:absolute md:right-[32px] md:top-0 md:bottom-0 md:m-auto flex items-center gap-1 md:gap-2 text-sm font-bold text-darkblue">
           <img src="/assets/shared/icon-comments.svg" alt="icon comments" />
           {productRequest.comments?.length > 0
             ? productRequest.comments.length +
